e52f9a6c4fd554a47dc6902e06b9c15a37dcb5ed
Jaseempk <jasimpk011@gmail.com>
2024-02-21 16:13:53 +0530

    more

--- src/ERC20Lock.sol
+++ src/ERC20Lock.sol
@@ -6,15 +6,19 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 //Error
 error ELOCK__InsufficientTokenBalance();
 error ELOCK__InvalidAddress();
+error ELOCK__InvalidChain();
 
 contract ERC20Lock {
     IERC20 public immutable i_tokenToLock;
 
     struct Token {
         address sender;
+        string chainId;
         uint256 amount;
-        address destAddr;
+        address mirroreTokenAddress;
     }
+
+    mapping(address => Token) public addressToBridge;
 
     event tokenBridgeDone(Token someTokenBridge);
 
@@ -22,18 +26,30 @@ contract ERC20Lock {
         i_tokenToLock = _tokenToLock;
     }
 
-    function bridgeToken(address _destAddr, uint256 _amountToBridge) public {
+    function bridgeToken(
+        address _destAddr,
+        uint256 _amountToBridge,
+        string memory chainId
+    ) public {
+        string memory acceptedChain = "devnet";
+
         if (
             i_tokenToLock.balanceOf(msg.sender) < _amountToBridge ||
             i_tokenToLock.balanceOf(msg.sender) == 0
         ) revert ELOCK__InsufficientTokenBalance();
+        if (
+            keccak256(abi.encodePacked(chainId)) !=
+            keccak256(abi.encodePacked(acceptedChain))
+        ) revert ELOCK__InvalidChain();
 
         if (_destAddr == address(0)) revert ELOCK__InvalidAddress();
         Token memory tokenBridge = Token(
             msg.sender,
+            chainId,
             _amountToBridge,
             _destAddr
         );
+        addressToBridge[msg.sender] = tokenBridge;
 
         i_tokenToLock.transfer(address(this), _amountToBridge);
         emit tokenBridgeDone(tokenBridge);
