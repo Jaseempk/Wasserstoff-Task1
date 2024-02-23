e16995b4fcb66439ef8612329c92c964f5767f1f
Jaseempk <jasimpk011@gmail.com>
2024-02-23 15:39:29 +0530

    not there yet...

--- src/ERC20Lock.sol
+++ src/ERC20Lock.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 //Error
 error ELOCK__InsufficientTokenBalance();
-error ELOCK__InvalidAddress();
+error ELOCK__InvalidTokenAddress();
 error ELOCK__InvalidChain();
 
 contract ERC20Lock {
@@ -13,45 +13,68 @@ contract ERC20Lock {
 
     struct Token {
         address sender;
+        address destToken;
         string chainId;
-        uint256 amount;
-        address mirroreTokenAddress;
+        uint256 ercAmount;
     }
 
-    mapping(address => Token) public addressToBridge;
+    mapping(address => mapping(bytes32 => Token)) public bridgeDetails;
+    mapping(address => uint256) public balances;
 
-    event tokenBridgeDone(Token someTokenBridge);
+    event tokenBridgeDone(
+        address sender,
+        address destToken,
+        string chainId,
+        uint256 ercAmount
+    );
+
+    modifier validateLock(address token, uint256 amount) {
+        if (token == address(0)) revert ELOCK__InvalidTokenAddress();
+        if (amount == 0) revert ELOCK__InsufficientTokenBalance();
+        _;
+    }
 
     constructor(IERC20 _tokenToLock) {
         i_tokenToLock = _tokenToLock;
     }
 
     function bridgeToken(
-        address _destAddr,
-        uint256 _amountToBridge,
-        string memory chainId
-    ) public {
+        Token memory newTransfer
+    ) public validateLock(newTransfer.destToken, newTransfer.ercAmount) {
         string memory acceptedChain = "devnet";
 
         if (
-            i_tokenToLock.balanceOf(msg.sender) < _amountToBridge ||
-            i_tokenToLock.balanceOf(msg.sender) == 0
-        ) revert ELOCK__InsufficientTokenBalance();
-        if (
-            keccak256(abi.encodePacked(chainId)) !=
+            keccak256(abi.encodePacked(newTransfer.chainId)) !=
             keccak256(abi.encodePacked(acceptedChain))
         ) revert ELOCK__InvalidChain();
 
-        if (_destAddr == address(0)) revert ELOCK__InvalidAddress();
-        Token memory tokenBridge = Token(
-            msg.sender,
-            chainId,
-            _amountToBridge,
-            _destAddr
-        );
-        addressToBridge[msg.sender] = tokenBridge;
+        // if (_destAddr == address(0)) revert ELOCK__InvalidAddress();
+        // Token memory tokenBridge = Token(
+        //     msg.sender,
+        //     chainId,
+        //     _amountToBridge,
+        //     _destAddr
+        // );
+        balances[newTransfer.sender] += newTransfer.ercAmount;
 
-        i_tokenToLock.transfer(address(this), _amountToBridge);
-        emit tokenBridgeDone(tokenBridge);
+        bytes32 bridgeId = keccak256(
+            abi.encodePacked(
+                block.timestamp,
+                newTransfer.destToken,
+                newTransfer.sender
+            )
+        );
+        bridgeDetails[msg.sender][bridgeId] = newTransfer;
+
+        uint256 amountToSend = newTransfer.ercAmount;
+        newTransfer.ercAmount = 0;
+
+        i_tokenToLock.transfer(address(this), amountToSend);
+        emit tokenBridgeDone(
+            newTransfer.sender,
+            newTransfer.destToken,
+            newTransfer.chainId,
+            amountToSend
+        );
     }
 }
